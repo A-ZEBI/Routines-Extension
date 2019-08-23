8f739781396c9f0c284225399689bc3bdb783ad5
Amir Hameed <picwellwisher12pk@gmail.com>
2019-08-23 20:15:51 +0500

    added one extra case. and improved a logic about find the right button for paid and free courses

--- content.js
+++ content.js
@@ -1,3 +1,35 @@
+//Paid
+//btn btn-lg btn-primary mt0 course-cta course-cta--add-to-cart disabled
+//btn btn-lg btn-quaternary btn-block ellipsis course-cta course-cta--buy
+
+
+
+
+//themoviedb.org API key with url
+//https://api.themoviedb.org/3/movie/550?api_key=b9374fb215013c460e3b9ea044294aa1
+//https://api.themoviedb.org/3/movie/343611?api_key={api_key}
+/* {
+  "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
+  "adult": false,
+  "overview": "Jack Reacher must uncover the truth behind a major government conspiracy in order to clear his name. On the run as a fugitive from the law, Reacher uncovers a potential secret from his past that could change his life forever.",
+  "release_date": "2016-10-19",
+  "genre_ids": [
+    53,
+    28,
+    80,
+    18,
+    9648
+  ],
+  "id": 343611,
+  "original_title": "Jack Reacher: Never Go Back",
+  "original_language": "en",
+  "title": "Jack Reacher: Never Go Back",
+  "backdrop_path": "/4ynQYtSEuU5hyipcGkfD6ncwtwz.jpg",
+  "popularity": 26.818468,
+  "vote_count": 201,
+  "video": false,
+  "vote_average": 4.19
+} */
 function ready(callback){
     /* in case the document is already rendered*/
 
@@ -30,9 +62,14 @@ ready(function(){
    }
    //Enroll
   else if(!window.location.pathname.includes('/cart/checkout')){
-
-      if (document.querySelector('a.course-cta')) {
-          document.querySelector('a.course-cta').click();
+      //Close window if course is paid
+      if(document.querySelector('.course-cta--buy')){
+        if(document.querySelector('.course-cta--buy').textContent.includes("Buy now")) window.close();
+      }
+      //Click on enroll now button
+      if (document.querySelector('.course-cta')) {
+          var btn = document.querySelector('.course-cta');
+          if(btn.textContent.includes("Enroll now")) btn.click();
       }
       else if(document.querySelector('a.fasc-button')){
 
@@ -40,10 +77,10 @@ ready(function(){
           var btn_href = btn.href;
           if(btn_href.includes("udemy")){
             console.log("match");
-            document.querySelector('a.fasc-button').click();
+            location.href = btn_href; //THis will open the udemy page in current page instead of new tab
           }else{
             console.log("not match");
-          } 
+          }
       }
   }
   //Still need to check few more cases.
